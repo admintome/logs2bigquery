7e3c3789e55c2bf5dee407d23aa225f84143425d
Bill Ward <bill@admintome.com>
2018-07-30 16:07:31 -0500

    fixed bug

--- query.py
+++ query.py
@@ -2,10 +2,10 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 query_job = client.query("""
-SELECT * FROM `admintome-bigdata-test.www_logs.access_logs`
+SELECT * FROM www_logs.access_logs
 """)
 results = query_job.result()
 for row in results:
-    print("Request: {}".format(row.request()))
+    print("Request: {}".format(row))
 if results.total_rows == 0:
     print("No logs returned")
